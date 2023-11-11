75e775d7894c36ac6518e49b2c249e80905e48af
alejandro yunes <alejandroyunes@outlook.com>
2023-11-11 06:56:31 -0500

    fixing erros

--- src/components/BillboardTwo/svg/Checkmark.tsx
+++
@@ -1,7 +0,0 @@
-export default function Checkmark() {
-  return (
-    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#ae3c43" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  );
-}
