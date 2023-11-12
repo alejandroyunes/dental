a31f30cbc8e2174bd0e48f24b85e82d89568cfe7
alejandro yunes <alejandroyunes@outlook.com>
2023-11-12 14:56:07 -0500

    addigng comparative

--- src/components/CompartiveProducts/ComparativeProducts.tsx
+++ src/components/CompartiveProducts/ComparativeProducts.tsx
@@ -1,14 +1,73 @@
 import Title from "../Title/Title"
-import { Container } from "./comparative.styled"
+import { Comparative, Container } from "./comparative.styled"
+import image from '../../pages/categories/best-irrigators/data/water.jpg'
 
 type Props = {
   title: string
   message: string
 }
+
+const data = [
+  {
+    model: 'Waterpik Ultra 660',
+    image: image,
+    prime: true,
+    dimenstions: '15 x 16,5 x 23 cm',
+    replacementHeads: 6,
+    deposit: '650 ml',
+    highlight: 'Economical, easy to use, and very comprehensive.',
+    problem: 'The customer service of Waterpik.',
+    price: 'Best buy',
+    satisfaction: '5/5',
+    view: 'View Price',
+  }
+]
+
 export default function ({ title, message }: Props) {
   return (
     <Container>
       <Title titleH2={title} message={message} />
+      <Comparative>
+        {data.map((item, index) => {
+          return (
+            <>
+              <div>
+                <div className="grid-item">
+                  <p>Model</p>
+                </div>
+
+                <div className="grid-item">
+                  <p>Image</p>
+                </div>
+
+              </div>
+
+              <div>
+                <div className="grid-item">
+                  <p>{item.model}</p>
+                </div>
+
+                <div className="grid-item">
+                  <img src={item.image} />
+                </div>
+              </div>
+
+
+
+              {/* <p className="grid-item">Imagen</p>
+                <p className="grid-item">Prime</p>
+                <p className="grid-item">Dimensions</p>
+                <p className="grid-item">Replacement heads</p>
+                <p className="grid-item">Capacity</p>
+                <p className="grid-item">Highlight</p>
+                <p className="grid-item">Problem</p>
+                <p className="grid-item">Price</p>
+                <p className="grid-item">Satisfaction</p> */}
+
+            </>
+          )
+        })}
+      </ Comparative>
     </Container>
   )
 }
--- src/components/CompartiveProducts/comparative.styled.ts
+++ src/components/CompartiveProducts/comparative.styled.ts
@@ -1,5 +1,65 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
+  width: 100%;
+  background-color: ${({ theme }) => theme.palette.primary.contrastBg};
+`
 
+export const Comparative = styled.div`
+  max-width:  ${({ theme }) => theme.breakpoint.sm};
+  margin: 0 auto;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  gap: 10px;
+  outline: 1px solid red;
+  a {
+    text-decoration: none;
+  }
+  h3 {
+    font-size: 1.4rem;
+    font-weight: 600;
+    margin: 0 0 16px;
+    line-height: 1.3;
+    max-width: 660px;
+    margin: 16px auto;
+    text-align: center;
+    color: ${({ theme }) => theme.textColor};
+  }
+  p {
+    font-size: 1.125rem;
+    line-height: 1.5;
+    max-width: 660px;
+    margin: 0 auto;
+    text-align: center;
+    color: ${({ theme }) => theme.textColor};
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.sm}) {
+    grid-template-columns: repeat(1, auto);
+    grid-template-rows: repeat(3, auto);
+  }
+
+  .grid-item {
+      border: 1px solid green; 
+      padding: 10px; 
+      text-align: center;
+      color: red;
+      align-items: center;
+      height: 100%;
+  }
+  
+  .child-card {
+    margin: 24px 0;
+    padding: 0 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  /* img {
+    max-width: 100%;
+    height: auto;
+    cursor: pointer;
+  } */
 `
